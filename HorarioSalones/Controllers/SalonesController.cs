1154d0da0263631f9ee22bd67dcfbe99d8cf3dc3
Monica Neri <mneri0918g@gmail.com>
2025-05-30 19:02:20 -0600

    Agregar archivos de proyecto.

---
+++ HorarioSalones/Controllers/SalonesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HorarioSalones.Controllers
+{
+    public class SalonesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
